a3099a19209e0dc07f33b873a99c1083964f19f6
kvgallagher <kvg0@mit.edu>
2020-05-08 16:06:32 -0400

    test paths

--- model_data/zip_codes.py
+++ model_data/zip_codes.py
@@ -1,3 +1,3 @@
-with open("mit_zip_code_list.txt") as f:
+with open("model_data/mit_zip_code_list.txt") as f:
     ZIP_CODES = f.read().splitlines()
 ZIP_CODES_PMF = {b: 1.0/len(ZIP_CODES) for b in sorted(ZIP_CODES)}
